688f157d4127e31793939c4a987e3f532bdd1f75
gulia <vgulia@hawk.iit.edu>
2015-12-15 22:33:47 -0700

    updated code a bit and commented for security for a bit.

--- src/main/java/com/barley/security/WebSecurityConfig.java
+++ src/main/java/com/barley/security/WebSecurityConfig.java
@@ -1,15 +1,11 @@
-package com.barley.security;
+/**package com.barley.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebSecurity
@@ -38,4 +34,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("vikram.gulia").password("risalo87").roles("USER");
     }
-}
+}*/
